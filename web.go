e98f7700bb0459dcb17e4e6c95cd8a11f6b3ea28
Abhi Agarwal <me@abhiagarwal.com>
2016-12-03 16:16:22 -0500

    small test

--- web.go
+++ web.go
@@ -4,22 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
+
+	nError "github.com/news-ai/web/errors"
 )
 
 func verifySMTP(w http.ResponseWriter, r *http.Request) {
 	// fmt.Println(VerifySMTPAccount("mail.privateemail.com:465", "hi@abhi.co", ""))
 
-	r.ParseForm()       // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form) // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+	switch r.Method {
+	case "POST":
+		fmt.Fprintf(w, "Hello")
+		return
 	}
-	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
+
+	nError.ReturnError(w, http.StatusInternalServerError, "SMTP error", "method not implemented")
+	return
 }
 
 func main() {
